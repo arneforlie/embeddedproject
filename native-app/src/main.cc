b284c2708e7d4559ca4207c59a223c18d500f95e
arneforlie <32863339+arneforlie@users.noreply.github.com>
2017-10-18 13:57:05 +0200

    Update main.cc

--- native-app/src/main.cc
+++ native-app/src/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 
 #include "mathy.h"
 #include "version.h"
@@ -16,6 +17,10 @@ int main(const int argc, const char *argv[])
       }
     }
     
+    printf("VERSION_STRING=%s\n", VERSION_STRING);
+    printf("BUILD_STRING=%s\n", BUILD_STRING);
+    printf("PRODUCT_STRING=%s\n", PRODUCT_STRING);
+
     printf("7 + 3 = %f\n", Mathy::Calculate::Add(5, 3));
     printf("7 - 3 = %f\n", Mathy::Calculate::Subtract(5, 3));
     printf("7 * 3 = %f\n", Mathy::Calculate::Multiply(5, 3));
